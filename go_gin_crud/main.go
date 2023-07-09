6852bb408e48e4ce84671b55f8daa1ed0962ab2b
knowave <kwaktaemin98@gmail.com>
2023-07-09 22:09:13 +0900

    gin framework test

--- go_gin_crud/main.go
+++ go_gin_crud/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+    r := gin.Default()
+    r.GET("", func(c *gin.Context) {
+        c.String(http.StatusOK, "Hello, World!")
+    })
+
+    r.Run("localhost:3000")
 }
