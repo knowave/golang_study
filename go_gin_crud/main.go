4bcdc5026bf3ab0a7451e04d64851255d86e8ef5
knowave <kwaktaemin98@gmail.com>
2023-07-18 15:45:29 +0900

    main

--- go_gin_crud/main.go
+++ go_gin_crud/main.go
@@ -1,10 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
-
+	"os"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
+
+var (
+    dbHost     string
+	dbPassword string
+	dbUsername string
+	dbName     string
+	dbPort     string
+)
+
+func init() {
+    loadErr := godotenv.Load()
+
+    if loadErr != nil {
+        fmt.Printf("end error %+v", loadErr)
+    }
+
+    dbHost = os.Getenv("DB_HOST")
+    dbPassword = os.Getenv("DB_PASSWORD")
+    dbUsername = os.Getenv("DB_USERNAME")
+    dbName = os.Getenv("DB_NAME")
+    dbPort = os.Getenv("DB_PORT")
+}
 
 func main() {
     r := gin.Default()
