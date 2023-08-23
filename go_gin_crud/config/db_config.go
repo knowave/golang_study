f9d1d6b845028222f84f7273694a62798172ad75
knowave <kwaktaemin98@gmail.com>
2023-08-23 17:25:14 +0900

    db config

---
+++ go_gin_crud/config/db_config.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"os"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/joho/godotenv"
+)
+
+var DB *gorm.DB
+
+func init() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		panic("Error Loading .env File")
+	}
+
+	dbUsername := os.Getenv("DB_USERNAME")
+    dbPassword := os.Getenv("DB_PASSWORD")
+    dbName := os.Getenv("DB_NAME")
+    dbHost := os.Getenv("DB_HOST")
+    dbPort := os.Getenv("DB_PORT")
+
+	DB, err = gorm.Open("mysql", dbUsername+":"+dbPassword+"@tcp("+dbHost+":"+dbPort+")/"+dbName+"?charset=utf8&parseTime=True&loc=Local")
+    if err != nil {
+        panic("Failed to connect to database")
+    }
+}
--- go_gin_crud/main.go
+++ go_gin_crud/main.go
@@ -1,51 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-	"os"
-
-	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
-	"github.com/knowave/golang_study/tree/main/go_gin_crud/domain/service"
-	"github.com/knowave/golang_study/tree/main/go_gin_crud/infra/persistence"
-)
-
-var (
-    dbHost     string
-	dbPassword string
-	dbUsername string
-	dbName     string
-	dbPort     string
-)
-
-func init() {
-    loadErr := godotenv.Load()
-
-    if loadErr != nil {
-        fmt.Printf("end error %+v", loadErr)
-    }
-
-    dbHost = os.Getenv("DB_HOST")
-    dbPassword = os.Getenv("DB_PASSWORD")
-    dbUsername = os.Getenv("DB_USERNAME")
-    dbName = os.Getenv("DB_NAME")
-    dbPort = os.Getenv("DB_PORT")
-}
+import "fmt"
 
 func main() {
-    repositories, err := persistence.NewRepositories(dbHost, dbPort, dbUsername, dbName, dbPassword)
-	if err != nil {
-		fmt.Errorf("[Error] persistence.NewRepositories  %v", err)
-		return
-	}
-
-    userService := service.NewUserService(repositories)
-
-    r := gin.Default()
-    r.GET("", func(c *gin.Context) {
-        c.String(http.StatusOK, "Hello, World!")
-    })
-
-    r.Run("localhost:3000")
+	fmt.Println("hello~")
 }
